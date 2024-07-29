d5b416eeddced834a3f5062a538e9cc6b5247b68
dasarishiv <dasarishiv@gmail.com>
2024-07-28 23:24:47 -0300

    booking integration

--- routers/bookingRouter.js
+++ routers/bookingRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { protectRoute } = require("../controllers/authController");
 const Razorpay = require("razorpay");
+const crypto = require("crypto");
 const bookingModel = require("../models/bookingModel");
 const User = require("../models/userModel");
 
@@ -10,65 +11,6 @@ require("dotenv").config();
 const instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_SECRET_KEY
-});
-
-bookingRouter.post("/:productId", protectRoute, async (req, res) => {
-  try {
-    const userId = req.userId;
-    const productId = req.params.productId;
-    const { priceAtBooking } = req.body;
-
-    const bookingObj = {
-      user: userId,
-      product: productId,
-      priceAtBooking
-    };
-
-    const booking = await bookingModel.create(bookingObj);
-
-    /** update user with booking details */
-    const user = await User.findById(userId);
-    user.bookings.push(booking._id);
-    await user.save();
-
-    /** creating order on razorpay */
-    var options = {
-      amount: priceAtBooking * 100, // amount in the smallest currency unit
-      currency: "INR",
-      receipt: booking._id.toString() // order id
-    };
-    const order = await instance.orders.create(options);
-    console.log("order created at razorpay", order);
-    booking.paymentOrderId = order.id;
-    await booking.save();
-    res.status(200).json({
-      message: "Order created successfully",
-      order: order
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
-    });
-  }
-});
-
-bookingRouter.get("/", protectRoute, async (req, res) => {
-  try {
-    const allBookings = await bookingModel
-      .find()
-      // .populate('user')
-      // .populate('product');
-      .populate({ path: "user", select: "name email" })
-      .populate({ path: "product", select: "name price" });
-    res.status(200).json({
-      message: "got all bookings",
-      data: allBookings
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
-    });
-  }
 });
 
 bookingRouter.post("/verify", async (req, res) => {
@@ -96,4 +38,90 @@ bookingRouter.post("/verify", async (req, res) => {
     console.log(err);
   }
 });
+
+bookingRouter.post("/:productId", protectRoute, async (req, res) => {
+  try {
+    console.log("calling booking router");
+    const userId = req.userId;
+    const productId = req.params.productId;
+    const { priceAtBooking } = req.body;
+
+    const bookingObj = {
+      user: userId,
+      product: productId,
+      priceAtBooking
+    };
+
+    const booking = await bookingModel.create(bookingObj);
+
+    console.log("booking created successfully:", booking);
+
+    /** update user with booking details */
+    const user = await User.findById(userId);
+    user.bookings.push(booking._id);
+    await user.updateOne({ _id: userId });
+
+    /** creating order on razorpay */
+    var options = {
+      amount: priceAtBooking * 100, // amount in the smallest currency unit
+      currency: "INR",
+      receipt: booking._id.toString() // order id
+    };
+    const order = await instance.orders.create(options);
+    console.log("order created at razorpay", order);
+    booking.paymentOrderId = order.id;
+    await booking.save();
+    res.status(200).json({
+      message: "Order created successfully",
+      order: {
+        ...order,
+        orderKey: process.env.RAZORPAY_KEY_ID,
+        companyName: "E-Cap"
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
+});
+
+bookingRouter.get("/", protectRoute, async (req, res) => {
+  try {
+    const allBookings = await bookingModel
+      .find()
+      // .populate('user')
+      // .populate('product');
+      .populate({ path: "user", select: "name email" })
+      .populate({ path: "product", select: "name price" });
+    res.status(200).json({
+      message: "got all bookings",
+      data: allBookings
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
+});
+
+bookingRouter.get("/:bookingId", protectRoute, async (req, res) => {
+  try {
+    const { bookingId } = req.params;
+    const bookingdata = await bookingModel
+      .findById(bookingId)
+      .populate({ path: "user", select: "name email" })
+      .populate({ path: "product", select: "name price" });
+
+    res.status(200).json({
+      message: "booking fetched successfully",
+      data: bookingdata
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
+});
+
 module.exports = bookingRouter;
